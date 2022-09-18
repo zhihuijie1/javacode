659fedbd8742f4aa8d396adf2406be32483a4f81
George Chen <92995461+zhihuijie1@users.noreply.github.com>
2022-09-18 21:14:19 +0800

    链表
    链表

---
+++ Java_22_9_18/src/demo2/Test.java
@@ -0,0 +1,20 @@
+package demo2;
+
+public class Test {
+
+    public void func(){
+
+    }
+    public static void func2(){
+
+
+    }
+    public void func3(){
+        func();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
