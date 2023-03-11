0d1f9de496908db4e3ca8c57d6969d443f08bfd5
西雅图的雪很美 <92995461+zhihuijie1@users.noreply.github.com>
2023-03-11 18:40:29 +0800

    2023.3.11

---
+++ P2/ThreadDemo2.java
@@ -0,0 +1,41 @@
+package P2;
+class Counter {
+    String name = "counter";
+    synchronized public void print() {
+        System.out.println(this.name);
+    }
+
+    synchronized public void add() {
+        System.out.println(name + "你好");
+    }
+}
+
+public class ThreadDemo2 {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    counter.print();
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    counter.add();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("结束");
+    }
+}
