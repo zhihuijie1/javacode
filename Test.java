4f440efac852203591608aae7c53c5a62d923f3d
西雅图的雪很美 <92995461+zhihuijie1@users.noreply.github.com>
2023-04-23 23:04:21 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,23 +1,77 @@
-package P1.package1;
+package P1.package4;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Test {
-    //这是一个main方法，是程序的入口：
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // File.separator:获取当前操作系统的拼接符。
+        // File f = new File("A:" + File.separator + "TEXT" + File.separator + "11.docx");
+        // File f1 = new File("A:" + File.separator + "TEXT" + File.separator + "11.docx");
+        File f = new File("A:\\TEXT\\11.docx");
+        File f1 = new File("A:\\TEXT\\11.docx");
+        File f2 = new File("A:\\TEXT\\12.docx"); // 随便写的一个目录
+        // 文件是否可读/文件是否可写
+        System.out.println(f.canRead()); // true
+        System.out.println(f.canWrite()); // true
+        System.out.println(f2.canWrite()); // false
+        System.out.println(f2.canRead()); // false
 
-        //创建Phone类的对象：
-        Phone p1 = new Phone("华为P40", 2035.98, 2020);
-        Phone p2 = new Phone("华为P40", 2035.98, 2020);
-        //比较两个对象：p1和p2对象：
-        //==的作用：比较左右两侧的值是否想的，要么相等，返回true,要么不相等,返回false
-        System.out.println(p1 == p2);//-->>>对于引用数据类型来说，比较的是地址值。--->一定返回的是false
+        // 获取文件的名字
+        System.out.println(f.getName()); // 11.docx
+        System.out.println(f1.getName()); // 11.docx
+        System.out.println(f2.getName()); // 12.docx
 
-        //Object类提供了一个方法 equals方法 ：作用：比较对象具体内容是否相等。
-        boolean flag = p1.equals(p2);//点进源码发现：底层依旧比较的是==，比较的还是地址值。
-        System.out.println(flag);
-        Cat cat = new Cat();
-        Boolean flag1 = p1.equals(cat);
-        System.out.println(flag1);
-        HuaWei huaWei = new HuaWei("华为P40", 2035.98, 2020);
-        System.out.println(huaWei.equals(p1));
+        // 获取当前文件的上级目录
+        System.out.println(f.getParent()); // A:\TEXT
+        System.out.println(f2.getParent()); // A:\TEXT
+
+        // 判断当前目录下的东西是否是一个文件
+        System.out.println(f.isFile()); // true
+        System.out.println(f2.isFile()); // false
+
+        // 判断当前目录下的东西是否是一个目录
+        System.out.println(f.isDirectory()); // false
+        System.out.println(f2.isDirectory()); // false
+
+        // 判断当前文件是否隐藏
+        System.out.println(f.isHidden()); // F
+        System.out.println(f2.isHidden()); // F
+
+        // 当前文件的大小
+        System.out.println(f.length()); // 14246
+        System.out.println(f2.length()); // 0
+
+        // 当前文件是否存在
+        System.out.println(f.exists()); // true
+        System.out.println(f2.exists()); // false
+
+        if (f2.exists()) {
+            f2.delete();
+        } else {
+            f2.createNewFile();
+        }
+
+        // 比较两个对象的地址是否一致
+        System.out.println(f == f1); // false -> 堆中的两个不同对象
+
+        // 比较两个对象对应的路径
+        System.out.println(f.equals(f1)); // true : 两个对象的路径是一样的。
+
+        // 根路径相关的
+        System.out.println("绝对路径: " + f.getAbsolutePath()); // 真实的一个精准的，完整的路径
+        System.out.println("相对路径: " + f.getPath()); // 有一个参照物，相对这个参照物的路径。
+        System.out.println("tostring: " + f.toString()); // tostring跟相对目录一致
+
+        //
+        File f5 = new File("demo.txt");
+        File f6 = new File("a/b/c/demo.txt");
+        if (!f5.exists()) {
+            f5.createNewFile();
+        }
+        System.out.println("绝对路径: " + f6.getAbsolutePath());
+        System.out.println("相对路径: " + f6.getPath());
+        System.out.println("tostring: " + f6.toString());
+
     }
 }
